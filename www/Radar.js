c76b14491213877e262c0d069a55dc0a44e32c89
Nick Patrick <nicholasjohnpatrick@gmail.com>
2020-06-15 06:06:15 -0400

    add JavaScript functions

--- www/Radar.js
+++ www/Radar.js
@@ -15,6 +15,10 @@ const setDescription = (description) => {
   exec('setDescription', [description]);
 };
 
+const setMetadata = (metadata) => {
+  exec('setMetadata', [metadata]);
+};
+
 const getPermissionsStatus = (callback) => {
   exec('getPermissionsStatus', null, callback);
 };
@@ -23,20 +27,32 @@ const requestPermissions = (background) => {
   exec('requestPermissions', [background]);
 };
 
-const startTracking = () => {
-  exec('startTracking');
+const getLocation = (callback) => {
+  exec('getLocation', null, callback);
+};
+
+const trackOnce = (arg1, arg2) => {
+  if (typeof arg1 === 'function') {
+    exec('trackOnce', null, arg1);
+  } else {
+    exec('trackOnce', [arg1], arg2);
+  }
+};
+
+const startTrackingEfficient = () => {
+  exec('startTrackingEfficient');
+};
+
+const startTrackingResponsive = () => {
+  exec('startTrackingResponsive');
+};
+
+const startTrackingContinuous = () => {
+  exec('startTrackingContinuous');
 };
 
 const stopTracking = () => {
   exec('stopTracking');
-};
-
-const trackOnce = (callback) => {
-  exec('trackOnce', null, callback);
-};
-
-const updateLocation = (location, callback) => {
-  exec('updateLocation', [location], callback);
 };
 
 const onEvents = (callback) => {
@@ -59,19 +75,75 @@ const offError = () => {
   exec('offEvents');
 };
 
+const getContext = (arg1, arg2) => {
+  if (typeof arg1 === 'function') {
+    exec('getContext', null, arg1);
+  } else {
+    exec('getContext', [arg1], arg2);
+  }
+};
+
+const searchPlaces = (options, callback) => {
+  exec('searchPlaces', [options], callback);
+};
+
+const searchGeofences = (options, callback) => {
+  exec('searchGeofences', [options], callback);
+};
+
+const searchPoints = (options, callback) => {
+  exec('searchPoints', [options], callback);
+};
+
+const autocomplete = (options, callback) => {
+  exec('autocomplete', [options], callback);
+};
+
+const geocode = (query, callback) => {
+  exec('geocode', [query], callback);
+};
+
+const reverseGeocode = (arg1, arg2) => {
+  if (typeof arg1 === 'function') {
+    exec('reverseGeocode', null, arg1);
+  } else {
+    exec('reverseGeocode', [arg1], arg2);
+  }
+};
+
+const ipGeocode = (callback) => {
+  exec('ipGeocode', null, callback);
+};
+
+const getDistance = (options, callback) => {
+  exec('getDistance', [options], callback);
+};
+
 const Radar = {
   setUserId,
   setDescription,
+  setMetadata,
   getPermissionsStatus,
   requestPermissions,
-  startTracking,
-  stopTracking,
+  getLocation,
   trackOnce,
-  updateLocation,
+  startTrackingEfficient,
+  startTrackingResponsive,
+  startTrackingContinuous,
+  stopTracking,
   onEvents,
   onError,
   offEvents,
   offError,
+  getContext,
+  searchPlaces,
+  searchGeofences,
+  searchPoints,
+  autocomplete,
+  geocode,
+  reverseGeocode,
+  ipGeocode,
+  getDistance,
 };
 
 module.exports = Radar;
